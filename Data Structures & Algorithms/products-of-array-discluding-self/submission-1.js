f4222a07d3fd73cfa875def471b3486cb1098128
Madeeha Khan <madeehakhan16@gmail.com>
2026-07-10 15:31:57 -0400

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const prefix = []
+        const suffix = []
+        const output = []
+        const n = nums.length
+        prefix[0] = 1
+        suffix[n-1] = 1
+
+        for(let i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] * nums[i-1]
+        }
+
+        for(let i = n-2; i >= 0; i--) {
+            suffix[i] = suffix[i+1] * nums[i+1]
+        }
+
+        for(let i = 0; i < n; i++) {
+            output[i] = prefix[i] *suffix[i]
+        }
+
+        return output
+    }
+}
