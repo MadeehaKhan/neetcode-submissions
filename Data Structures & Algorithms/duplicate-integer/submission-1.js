01703cd31ae098618e2018b2d651840683cd84f8
Madeeha Khan <madeehakhan16@gmail.com>
2026-07-10 22:05:56 -0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+
+        const setNums = new Set(nums)
+
+        return (!(setNums.size===nums.length))
+    }
+}
