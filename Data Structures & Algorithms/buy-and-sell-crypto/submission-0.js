811a2506c1e81ca6d9218541f7dd7c3f827e3e16
Madeeha Khan <madeehakhan16@gmail.com>
2026-07-14 18:36:00 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0
+        let left = 0
+        let right = 1
+
+        while(right < prices.length) {
+             if(prices[right] > prices[left]) {
+                maxProfit = Math.max(prices[right]-prices[left], maxProfit)
+             }
+             else {
+                left = right
+             }
+             right++
+        }
+
+        return maxProfit
+    }
+}
